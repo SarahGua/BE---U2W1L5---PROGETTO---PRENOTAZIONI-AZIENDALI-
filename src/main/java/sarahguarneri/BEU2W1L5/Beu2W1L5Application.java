747ef34025e1172b3381753e0338ac7b1be7e5e7
SarahGuarneri <sarahguarneri97@icloud.com>
2024-01-12 10:29:57 +0100

    FILE

---
+++ src/main/java/sarahguarneri/BEU2W1L5/Beu2W1L5Application.java
@@ -0,0 +1,13 @@
+package sarahguarneri.BEU2W1L5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beu2W1L5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Beu2W1L5Application.class, args);
+	}
+
+}
---
+++ src/test/java/sarahguarneri/BEU2W1L5/Beu2W1L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package sarahguarneri.BEU2W1L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Beu2W1L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
